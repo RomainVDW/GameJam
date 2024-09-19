9ee07483e5ff68f76f1d3c58650335976a3cb6ed
Grengaar <drayol.nrf@gmail.com>
2024-09-19 16:17:56 +0200

    Update CameraController and Enemy

--- Assets/Code/Script/CameraController.cs
+++ Assets/Code/Script/CameraController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    Transform _player;
+    private Transform _player;
     public Vector3 posOffset;
     private Vector3 velocity = Vector3.zero;
-    void Awake()
+    private float _smoothPositionTime = 0.3f;
+    void Start()
     {
-        GameManager.s_Instance.Player = _player;
+        _player = GameManager.s_Instance.Player;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        Vector3 position = Vector3.SmoothDamp(transform.position, _player.transform.position - posOffset, ref velocity, Time.deltaTime);
+        Vector3 position = Vector3.SmoothDamp(transform.position, _player.transform.position - posOffset, ref velocity, _smoothPositionTime);
+        transform.position = position;
     }
 }
--- Assets/Code/Script/EnemyParent.cs
+++ Assets/Code/Script/EnemyParent.cs
@@ -6,15 +6,21 @@ using UnityEngine;
 public class EnemyParent : MonoBehaviour
 {
     public float _health;
-    public Transform _player;
-    void Awake()
+    private Transform _player;
+    private Rigidbody _rb;
+    private Vector3 _moveDirection;
+    public float _speed = 2f;
+    void Start()
     {
         _player = GameManager.s_Instance.Player;
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 direction = _player.transform.position - transform.position;
+        _moveDirection = direction;
+        _rb.velocity = new Vector3(_moveDirection.x, transform.position.y, _moveDirection.z) * _speed;
     }
 }
