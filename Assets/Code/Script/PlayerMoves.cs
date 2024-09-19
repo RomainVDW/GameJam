fda4b83d6df6a0fc89ffd9eca831189d8e65ea00
Justin François <justin.francois@student.heaj.be>
2024-09-19 15:37:47 +0200

    Improve player moves

--- Assets/Code/Script/PlayerMoves.cs
+++ Assets/Code/Script/PlayerMoves.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerMoves : MonoBehaviour
 {
     [SerializeField] private CharacterController _playerCtrlr;
-    private InputActionAsset _action;
+    [SerializeField] private InputActionAsset _action;
+    [Header("Parameters")]
+    [SerializeField] private float _playerSpeed = 10;
     private InputAction _move;
 
     // Start is called before the first frame update
@@ -14,6 +16,7 @@ public class PlayerMoves : MonoBehaviour
     {
         GameManager.s_Instance.Player = transform;
         _move = _action.FindAction("Move");
+        //_action.FindActionMap("Gameplay").enabled = true;
     }
 
     // Update is called once per frame
@@ -24,6 +27,9 @@ public class PlayerMoves : MonoBehaviour
 
     private void MovePlayer()
     {
-        _playerCtrlr.SimpleMove(_move.ReadValue<Vector2>());
+        Vector2 inputValue = _move.ReadValue<Vector2>();
+        Vector3 playerDirection = new Vector3(inputValue.x, 0, inputValue.y) * _playerSpeed;
+        _playerCtrlr.SimpleMove(playerDirection);
+        print(playerDirection);
     }
 }
