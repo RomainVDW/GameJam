c44368351c97dc902655e51e1f3f5a810baf54cd
Luxete <145855017+Luxete02@users.noreply.github.com>
2024-09-23 16:46:38 +0200

    Correcting Scypthe

--- Assets/Code/Script/Scythe.cs
+++ Assets/Code/Script/Scythe.cs
@@ -15,7 +15,7 @@ public class Scythe : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        other.gameObject.TryGetComponent(out PlayerHealth player);
+        other.gameObject.TryGetComponent(out IHealth player);
         player.TakeDamage(1.0f);
     }
 }
